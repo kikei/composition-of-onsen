2428cf5b7210577cd292723664af51a291ca04d9
kikei <fujii@xaxxi.net>
2020-05-05 04:29:07 +0900

    support quality name for 鉱泉 (kosen; mineral spring)

--- app/src/utils/OnsenUtil.tsx
+++ app/src/utils/OnsenUtil.tsx
@@ -16,7 +16,13 @@ type Temperature =
 type OsmoticPressure =
     'Hypotonic' | 'Isotonic' | 'Hypertonic' | 'Unknown';
 
-type ToLocalQualityName= (comp: Comp, simple: boolean) => string | undefined;
+interface ILocalizerOption {
+    simple?: boolean,
+    mineral?: boolean
+}
+
+type ToLocalQualityName = (comp: Comp, options: ILocalizerOption)
+=> string | undefined;
 
 function categorizePh(ph: number): AcidityAlkalinity {
     return ph < 3.0 ? 'Acid' :
@@ -52,6 +58,25 @@ function isMedicalI(mg: number): boolean { return mg >= 10.0 }
 function isMedicalS(mg: number): boolean { return mg >= 2.0 }
 // function isMedicalRn(mg: number): boolean { return mg >= 3e-9 }
 
+function isMineralCO2(mg: number): boolean { return mg >= 250.0 }
+function isMineralHSiO3(mg: number): boolean { return mg >= 50.0 }
+function isMineralH2SiO3(mg: number): boolean { return mg >= 50.0 }
+function isMineralBO2(mg: number): boolean { return mg >= 5.0 }
+function isMineralHBO2(mg: number): boolean { return mg >= 5.0 }
+function isMineralI(mg: number): boolean { return mg >= 1.0 }
+// function isMineralH(mg: number): boolean { return mg >= 1.0 }
+// function isMineralLi(mg: number): boolean { return mg => 1.0 }
+// function isMineralSr(mg: number): boolean { return mg => 10.0 }
+// function isMineralBa(mg: number): boolean { return mg => 5.0 }
+// function isMineralFe(mg: number): boolean { return mg >= 10.0 }
+// function isMineralMn(mg: number): boolean { return mg >= 10.0 }
+// function isMineralBr(mg: number): boolean { return mg >= 5.0 }
+function isMineralF(mg: number): boolean { return mg >= 2.0 }
+// function isMineralHAsO2(mg: number): boolean { return mg >= 1.3 }
+// function isMineralHAsO2(mg: number): boolean { return mg >= 1.0 }
+// function isMineralS(mg: number): boolean { return mg >= 1.0 }
+// function isNaHCO2(mg: number): boolean { return mg => 340 }
+
 // 硫化水素型
 function isTypeHS(mmolHS: number, mmolS2O3: number, mmolH2S: number): boolean {
     return mmolH2S > mmolHS + mmolS2O3;
@@ -67,18 +92,29 @@ function isSimple(a: Analysis): boolean {
 
 class OnsenQualityNameBuilder {
     special: Array<Comp>;
+    mineral: Array<Comp>;
     positiveIon: Array<Comp>;
     negativeIon: Array<Comp>;
     typeHS: boolean;
+    isMineral: boolean;
     constructor() {
         this.special = [];
+        this.mineral = [];
         this.positiveIon = [];
         this.negativeIon = [];
+        this.isMineral = false;
         this.typeHS = false;
+    }
+    getSpecial(): Array<Comp> {
+        return this.special;
     }
     addSpecial(key: Comp): void {
         if (!this.special.includes(key))
             this.special.push(key);
+    }
+    addMineral(key: Comp): void {
+        if (!this.mineral.includes(key))
+            this.mineral.push(key);
     }
     addPositiveIon(key: Comp): void {
         if (!this.positiveIon.includes(key))
@@ -91,18 +127,30 @@ class OnsenQualityNameBuilder {
     setTypeHS(typeHS: boolean): void {
         this.typeHS = typeHS;
     }
+    setMineral(isMineral: boolean): void {
+        this.isMineral = isMineral;
+    }
+    buildForMineral(dict: ToLocalQualityName): string {
+        const p0 = applyDict(this.mineral, dict, { mineral: true });
+        return '温泉法第二条の別表中に示された' +
+               p0.join('及び') +
+               'の項で温泉法の温泉に適合する';
+    }
     build(simple: boolean,
           osmoticPressure: OsmoticPressure,
           acidAlka: AcidityAlkalinity,
           temperature: Temperature,
           dict: ToLocalQualityName)
     : string {
+        if (this.isMineral) {
+            return this.buildForMineral(dict);
+        }
         let p0 = [] as Array<string>;
         let p1 = [] as Array<string>;
         let p2 = [] as Array<string>;
         const p3 = [] as Array<string>;
         if (simple) {
-            p2 = applyDict(this.special, dict, simple) || ['温泉'];
+            p2 = applyDict(this.special, dict, { simple: true }) || ['温泉'];
             p2 = ['単純' + p2.join('')];
         } else {
             p0 = applyDict(this.special, dict);
@@ -207,7 +255,26 @@ export function qualityName(a: Analysis): string {
     // if (isMedicalNaCl(a)){}
     // if (isMedicalHCO3(a)){}
     // if (isMedicalSO4(a)){}
+
+    // Mineral
+    if (isMineralCO2(a.gasValue(Comp.CO2)?.mg))
+        q.addMineral(Comp.CO2);
+    if (isMineralI(a.negativeIonValue(Comp.I)?.mg))
+        q.addMineral(Comp.I);
+    if (isMineralF(a.negativeIonValue(Comp.F)?.mg))
+        q.addMineral(Comp.F);
+    if (isMineralBO2(a.negativeIonValue(Comp.BO2)?.mg) ||
+        isMineralHBO2(a.undissociatedValue(Comp.HBO2)?.mg))
+        q.addMineral(Comp.HBO2);
+    if (isMineralHSiO3(a.negativeIonValue(Comp.HSiO3)?.mg) ||
+        isMineralH2SiO3(a.undissociatedValue(Comp.H2SiO3)?.mg))
+        q.addMineral(Comp.H2SiO3);
+
     const simple: boolean = isSimple(a);
+    if (q.special.length === 0 && simple) {
+        q.setMineral(true);
+    }
+
     if (!simple) {
         let totalPositiveIonMval: number = a.positiveIon.getTotal().mval;
         a.positiveIonList((a, b) => b.mvalPercent - a.mvalPercent)
@@ -227,7 +294,22 @@ export function qualityName(a: Analysis): string {
 };
 
 
-const qualityJPName: ToLocalQualityName = (comp: Comp, simple: boolean) => {
+const qualityJPName: ToLocalQualityName =
+    (comp: Comp, options: ILocalizerOption) =>
+{
+    const { simple, mineral } = options;
+    if (mineral) {
+        switch (comp) {
+            case Comp.I:
+                return 'ヨウ素イオン';
+            case Comp.F:
+                return 'ふっ素イオン';
+            case Comp.H2SiO3:
+                return 'メタケイ酸';
+            case Comp.HBO2:
+                return 'メタほう酸';
+        }
+    }
     if (simple) {
         switch (comp) {
             case Comp.CO2:
@@ -277,8 +359,13 @@ const qualityJPName: ToLocalQualityName = (comp: Comp, simple: boolean) => {
 
 
 function applyDict(comps: Array<Comp>, dict: ToLocalQualityName,
-                   simple: boolean = false) {
-    return comps.map(c => dict(c, simple))
+                   options?: ILocalizerOption | undefined) {
+    const opts = {
+        simple: false,
+        mineral: false,
+        ...options
+    };
+    return comps.map(c => dict(c, opts))
                 .filter(x => !!x)
                 .map((x: string | undefined) => !!x ? x : '');
 }
