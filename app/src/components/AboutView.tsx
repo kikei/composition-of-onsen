ebafec9e1699dfc2e03fe84405733746941b493b
kikei <fujii@xaxxi.net>
2020-07-17 22:43:05 +0900

    add AboutView and WhatIsAnalysisView

---
+++ app/src/components/AboutView.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const AboutView: React.FC<React.Props<any>> = _ => {
+    return (
+        <div className="content">
+            <h2 className="title is-4">温泉草子について</h2>
+            <div className="section">
+                準備中
+            </div>
+            <div className="section">
+                <h3 className="title is-5">お問い合わせ</h3>
+                fujii@xaxxi.net までお願いします。
+            </div>
+            <div className="section">
+                <h3 className="title is-5">関連ページ</h3>
+                <dl>
+                    <dt><a href="https://fontopo.com/">fontopo</a></dt>
+                    <dd>
+                        ロゴフォントには fontopo 様で配布されている
+                        <a href="https://fontopo.com/?p=377">はれのそら明朝</a>
+                        を利用しました。
+                    </dd>
+                    <dt><a href="https://fontawesome.com">Font Awesome</a></dt>
+                    <dd>
+                        Webページ内の各種アイコンは Font Awesome を利用しました。
+                    </dd>
+                    <dt><a href="https://bulma.io/">Bulma</a></dt>
+                    <dd>
+                        Webページデザイン、レイアウトには、
+                        CSSフレームワークの Bulma を利用しました。
+                    </dd>
+                    <dt><a href="https://github.com/kikei/composition-of-onsen">kikei/composition-of-onsen</a></dt>
+                    <dd>
+                        Webクライアント側アプリケーションはオープンソースで開発しています。
+                    </dd>
+                    <dt><a href="https://github.com/kikei/composion-of-onsen-server">kikei/composion-of-onsen-server</a></dt>
+                    <dd>
+                        サーバー側もオープンソースで開発しています。
+                    </dd>
+                </dl>
+            </div>
+        </div>
+    )
+}
+
+export default AboutView;
---
+++ app/src/components/WhatIsAnalysisView.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const WhatIsAnalysisView: React.FC<React.Props<any>> = _ => {
+    return (
+        <div className="content">
+            <h2 className="title is-4">温泉分析書について</h2>
+            <div className="section">
+                準備中
+            </div>
+            <div className="section">
+                <h3 className="title is-5">温泉分析書とは</h3>
+                準備中
+            </div>
+            <div className="section">
+                <h3 className="title is-5">当サイトの楽しみ方</h3>
+                準備中
+            </div>
+            {/*
+            <p>
+                「温泉分析書」と書いた何やら難しそうな表が、温泉施設にひっそりと掲示されているのを見たことがあるでしょうか？
+            </p>
+            <p>
+                この表を読むと、温泉のお湯の名前と、成分、特徴などが記載されており、
+                その温泉がどのような温泉か、おおまかに見取ることができます。
+            </p>
+            <p>
+                成分を理解して温泉に浸かれば、より深く入浴を楽しむことができるでしょう。
+            </p>
+            <p>
+                旅の思い出に、温泉の道の巡礼に、温泉分析書を収集してみてはいかがでしょうか？
+            </p>
+            */}
+        </div>
+    )
+}
+
+export default WhatIsAnalysisView;
